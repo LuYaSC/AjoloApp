ff459da8ac3137e4218a42b66b75df9ec7991f2d
Luis Jose Jimenez Peña <luisjimpe93@gmail.com>
2022-08-29 19:39:21 -0400

    SAP-1 Crating Collaborations base files

---
+++ SAP.RuleEngine/CollaboratorService/CollaboratorService.cs
@@ -0,0 +1,16 @@
+﻿using SAP.Core.Business;
+using SAP.Repository.SAPRepository;
+using SAP.Repository.SAPRepository.Entities;
+using System.Security.Principal;
+
+namespace SAP.RuleEngine.CollaboratorService
+{
+    public class CollaboratorService : BaseBusiness<Collaborator, SAPContext>, ICollaboratorService
+    {
+        public CollaboratorService(SAPContext context, IPrincipal userInfo) : base(context, userInfo)
+        {
+        }
+
+
+    }
+}
